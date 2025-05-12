5bde9a5155a326bf2b7cf15070c2e893ccc9749c
bachrul25 <bachrulasr03@gmail.com>
2025-05-12 15:55:38 +0700

    Update navbar with blue underline hover effect

--- fe-roompi/src/App.jsx
+++ fe-roompi/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Profile from './pages/Profile/profile';
+import Room from './pages/Room/Room';
+import BookingConfirmation from './pages/BookingConfirmation/BookingConfirmation';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import DetailRoom from './pages/DetailRoom/DetailRoom';
 
 function App() {
   return (
@@ -9,6 +14,13 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/detail-room" element={<DetailRoom/>} />
+        <Route path="/rooms" element={<Room />} />
+        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
+        
+
       </Routes>
     </Router>
   )
