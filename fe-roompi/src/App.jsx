3183a29057f8e0bed5a2bc9fd6b40b89ede03428
itsclarr25 <clarintapradyan@gmail.com>
2025-05-12 19:31:15 +0700

    Save Local Changes

--- fe-roompi/src/App.jsx
+++ fe-roompi/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Profile from './pages/Profile/profile';
+import History from './pages/History/history'; 
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/history" element={<History />} />
       </Routes>
     </Router>
   )
