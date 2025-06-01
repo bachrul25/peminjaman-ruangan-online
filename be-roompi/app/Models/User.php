3b14a2b55fb9f9415eb19160042b98b554a6f244
bachrul25 <bachrulasr03@gmail.com>
2025-06-01 21:49:45 +0700

    Fix conflict and update User model

--- be-roompi/app/Models/User.php
+++ be-roompi/app/Models/User.php
@@ -1,39 +1,14 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
 use Carbon\Carbon;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Collection;
+use App\Models\Pinjam;
 
-/**
- * Class User
- *
- * @property int $id
- * @property string $name
- *
- * @property int $id_user
- * @property string $role
- * @property string $nama
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string $telepon
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- *
- * @property Collection|Pinjam[] $pinjams
- *
- * @package App\Models
- */
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
@@ -59,6 +34,6 @@ class User extends Authenticatable
 
     public function pinjams()
     {
-        return $this->hasMany(Pinjam::class, 'id_user'); // atau 'user_iduser' tergantung relasi di tabel Pinjam
+        return $this->hasMany(Pinjam::class, 'id_user');
     }
 }
